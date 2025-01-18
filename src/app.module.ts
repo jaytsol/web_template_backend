515fecc4ccd4fec972bcb5c9999fc4a1768571e3
jaytsol <jaewhee@tictag.io>
2025-01-19 01:53:44 +0900

    added cacheManager

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, CacheModule.register()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
