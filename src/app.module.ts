95eddcd8a8936a0447c7900afb661059ea182223
jaytsol <jaewhee@tictag.io>
2025-01-19 02:27:49 +0900

    Added redis using docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,19 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { redisStore } from 'cache-manager-redis-store';
 
 @Module({
-  imports: [DatabaseModule, CacheModule.register()],
+  imports: [
+    DatabaseModule,
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      ttl: 100,
+    }),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
