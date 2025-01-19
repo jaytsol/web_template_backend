eda6f6d551684a05cb0c3a61ff51542b6219af1c
jaytsol <jaewhee@tictag.io>
2025-01-19 21:01:17 +0900

    handle jwt secret key at .env

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_ACCESS_TOKEN_SECRET_KEY,
+      signOptions: {
+        expiresIn: process.env.JWT_ACCESS_TOKEN_EXPIRES_IN || '7d',
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
