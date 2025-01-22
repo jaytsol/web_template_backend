e324def4a157d0b070c4480dbc67bb262dea55da
jaytsol <jaewhee@tictag.io>
2025-01-22 18:13:45 +0900

    added reflector to authGuard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,12 +8,14 @@ import {
   Post,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Public } from 'src/common/decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @HttpCode(HttpStatus.OK)
+  @Public()
   @Post('login')
   signIn(@Body() signInDto: Record<string, any>) {
     return this.authService.signIn(signInDto.username, signInDto.password);
