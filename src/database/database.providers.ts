2de6095578f76dcce6303178cc6d7adb27ffa24e
jaytsol <jaewhee@tictag.io>
2025-01-28 18:20:34 +0900

    constantized

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,11 +1,12 @@
 import * as mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
+import { DATABASE_CONNECTION } from 'src/providers/constants';
 
 dotenv.config();
 
 export const databaseProviders = [
   {
-    provide: 'DATABASE_CONNECTION',
+    provide: DATABASE_CONNECTION,
     useFactory: (): Promise<typeof mongoose> =>
       mongoose.connect(process.env.MONGODB_URL),
   },
