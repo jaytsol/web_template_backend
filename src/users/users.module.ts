95038780f33bdd070b187a62f311ac2ef59b8a1b
jaytsol <jaewhee@tictag.io>
2025-01-28 18:31:58 +0900

    added swagger

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { userProviders } from 'src/providers/users/user.providers';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [UsersService, ...userProviders],
   exports: [UsersService],
   controllers: [UsersController],
