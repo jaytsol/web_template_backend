b5a16d29d207e6ca300470fe29246fcdd146254d
jaytsol <jaewhee@tictag.io>
2025-01-31 15:15:39 +0900

    fixed get all users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, Param, Request } from '@nestjs/common';
+import { Controller, Get, Request, Query } from '@nestjs/common';
 import { UserService } from './user.service';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { ObjectId } from 'mongoose';
+import { ApiBearerAuth, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { ObjectId, Types } from 'mongoose';
 
 @ApiBearerAuth()
 @ApiTags('User')
@@ -14,13 +14,19 @@ export class UserController {
     return this.userService.findOne(req.user.id);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: ObjectId) {
-    return this.userService.findOne(id);
-  }
-
   @Get('all')
   findAll() {
     return this.userService.findAll();
+  }
+
+  @Get()
+  @ApiQuery({
+    name: 'id',
+    required: true,
+    type: 'string',
+    example: `${new Types.ObjectId()}`,
+  })
+  findOne(@Query('id') id: ObjectId) {
+    return this.userService.findOne(id);
   }
 }
